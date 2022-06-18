5fd962a7a067ceed230cbcf00c5b5f9ecec38cae
Rishat Fayzullin <nilluziaf@gmail.com>
2022-06-18 23:37:24 +0300

    Feat(getting weather by GPS coords): updated config.py
    Added setting language

--- config.py
+++ config.py
@@ -1,3 +1,18 @@
 """Configurations of the application."""
 
+from enum import Enum
+
+
+class OpenWeatherLanguage(Enum):
+    """
+    Languages supported on Open Weather API Service.
+
+    Here only few of them.
+    List of all supported langs -> https://openweathermap.org/current#multi
+    """
+
+    ENGLISH = "en"
+    RUSSIAN = "ru"
+
+
 USE_ROUNDED_COORDS = True
