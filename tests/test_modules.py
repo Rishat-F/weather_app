fed8e67cd872edcf74de8f2d103e84dd08a4da40
Rishat Fayzullin <nilluziaf@gmail.com>
2022-06-19 01:33:29 +0300

    CI/CD(tests): added tests for coordinates.py

--- tests/test_modules.py
+++ tests/test_modules.py
@@ -1,6 +1,30 @@
-"""Tests for application."""
+"""Tests for application modules."""
+
+import numbers
+
+import pytest
+
+from coordinates import Coordinates, get_gps_coordinates
 
 
-def test_true() -> None:
-    """Temporary test just for setup Github Actions workflow."""
-    assert True
+@pytest.mark.xfail(reason="Getting current GPS coordinates dont work yet.")
+class TestGettingGpsCoordinates:
+    """Tests for coordinates.py module."""
+
+    def setup(self) -> None:
+        """Setup for all tests."""  # noqa
+        self.coordinates = get_gps_coordinates()
+
+    def test_get_gps_coordinates_returns_coordinates(self) -> None:
+        """Check get_gps_coordinates returns Coordinates type."""
+        assert isinstance(self.coordinates, Coordinates)
+
+    def test_coordinates_has_latitude_and_longitude(self) -> None:
+        """Check coordinates has latitude and longitude attributes."""
+        assert hasattr(self.coordinates, "latitude")
+        assert hasattr(self.coordinates, "longitude")
+
+    def test_coordinates_latitude_and_longitude_are_numbers(self) -> None:
+        """Check latitude and longitude are numbers."""
+        assert isinstance(self.coordinates.latitude, numbers.Real)
+        assert isinstance(self.coordinates.longitude, numbers.Real)
