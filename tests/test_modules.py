80d06516cd6a20989ca0c713e53bc8c742631a9b
Rishat Fayzullin <nilluziaf@gmail.com>
2022-06-19 02:12:29 +0300

    CI/CD(tests): added tests for weather_api_service.py

--- tests/test_modules.py
+++ tests/test_modules.py
@@ -1,10 +1,12 @@
 """Tests for application modules."""
 
 import numbers
+from datetime import datetime
 
 import pytest
 
 from coordinates import Coordinates, get_gps_coordinates
+from weather_api_service import Weather, WeatherType, get_weather
 
 
 @pytest.mark.xfail(reason="Getting current GPS coordinates dont work yet.")
@@ -28,3 +30,40 @@ class TestGettingGpsCoordinates:
         """Check latitude and longitude are numbers."""
         assert isinstance(self.coordinates.latitude, numbers.Real)
         assert isinstance(self.coordinates.longitude, numbers.Real)
+
+
+class TestGettingWeather:
+    """Tests for weather_api_service.py module."""
+
+    def setup(self) -> None:
+        """Setup for all tests."""  # noqa
+        coordinates = Coordinates(latitude=55.75, longitude=52.43)
+        self.weather = get_weather(coordinates)
+
+    def test_get_weather_returns_weather(self) -> None:
+        """Check get_weather returns Weather type."""
+        assert isinstance(self.weather, Weather)
+
+    def test_weather_has_right_attributes(self) -> None:
+        """Check weather has next attributes.
+
+        temperature, weather_type, weather_description,
+        wind_speed, sunrise, sunset, city
+        """
+        assert hasattr(self.weather, "temperature")
+        assert hasattr(self.weather, "weather_type")
+        assert hasattr(self.weather, "weather_description")
+        assert hasattr(self.weather, "wind_speed")
+        assert hasattr(self.weather, "sunrise")
+        assert hasattr(self.weather, "sunset")
+        assert hasattr(self.weather, "city")
+
+    def test_weather_attributes_types(self) -> None:
+        """Check weather attributes has right types."""
+        assert isinstance(self.weather.temperature, numbers.Real)
+        assert isinstance(self.weather.weather_type, WeatherType)
+        assert isinstance(self.weather.weather_description, str)
+        assert isinstance(self.weather.wind_speed, numbers.Real)
+        assert isinstance(self.weather.sunrise, datetime)
+        assert isinstance(self.weather.sunset, datetime)
+        assert isinstance(self.weather.city, str)
