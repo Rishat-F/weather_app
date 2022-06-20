22a223ce3ae3114fff037671928d6d5f15d18517
Rishat Fayzullin <nilluziaf@gmail.com>
2022-06-20 22:32:20 +0300

    CI/CD(tests): added tests for weather_api_service module exceptions
    Checks that weather_api_service.py raises exceptions
    in different situations as expected.
    Not realized yet and marked as xfail.

--- tests/test_exceptions.py
+++ tests/test_exceptions.py
@@ -2,7 +2,13 @@
 
 import pytest
 
-from exceptions import CantGetGpsCoordinates, CommandRunsTooLong, NoSuchCommand
+from exceptions import (
+    ApiServiceError,
+    CantGetGpsCoordinates,
+    CantGetWeather,
+    CommandRunsTooLong,
+    NoSuchCommand,
+)
 
 
 @pytest.mark.xfail(reason="not realized yet")
@@ -40,3 +46,40 @@ class TestCoordinatesModuleExceptions:
         Example: '{'latitude': 50.55, 'longitude': 50.55, qwerty}'.
         """
         assert isinstance(False, CantGetGpsCoordinates)
+
+
+@pytest.mark.xfail(reason="not realized yet")
+class TestWeatherApiServiceExceptions:
+    """Test exceptions raising while getting weather by GPS coordinates."""
+
+    def test_no_such_command(self) -> None:
+        """There is no command in system for getting weather by GPS coordinates."""
+        assert isinstance(False, NoSuchCommand)
+
+    def test_command_runs_too_long(self) -> None:
+        """Command for getting weather by GPS coordinates runs too long."""
+        assert isinstance(False, CommandRunsTooLong)
+
+    def test_command_has_stderr(self) -> None:
+        """Command for getting weather by GPS coordinates ends with err in stderr."""
+        assert isinstance(False, CantGetWeather)
+
+    def test_command_has_wrong_exit_code(self) -> None:
+        """Command for getting weather by GPS coordinates returns code != 0."""
+        assert isinstance(False, CantGetWeather)
+
+    def test_undecodable_command_output(self) -> None:
+        """Command stdout is undecodable."""
+        assert isinstance(False, CantGetWeather)
+
+    def test_command_output_has_invalid_dictionary(self) -> None:
+        """Command stdout has no valid dictionary with weather data."""
+        assert isinstance(False, ApiServiceError)
+
+    def test_command_output_has_incorrect_dictionary(self) -> None:
+        """
+        Dictionary with weather data is incorrect for json.loads().
+
+        Example: '{'temperature': 10, qwerty, 'wind': {'speed': 3.5}'.
+        """
+        assert isinstance(False, CantGetWeather)
