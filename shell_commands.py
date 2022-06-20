892d8ab3ca5e9fa25d8fe4c320a79b2b7d3b7171
Rishat Fayzullin <nilluziaf@gmail.com>
2022-06-20 20:58:39 +0300

    Bugfix: Fixed Circular Imports error
    GepGpsCoordinates class removed,
    parsing GPS coordinates function
    goes to coordinates.py module

--- shell_commands.py
+++ shell_commands.py
@@ -1,24 +1,15 @@
 """Shell commands used by application."""
 
-import json
-import re
-from json.decoder import JSONDecodeError
 from subprocess import PIPE, Popen, TimeoutExpired
 from typing import List, NamedTuple
 
-from coordinates import Coordinates
-from exceptions import (
-    CantGetGpsCoordinates,
-    CommandExecutionFailed,
-    CommandRunsTooLong,
-    NoSuchCommand,
-)
+from exceptions import CommandExecutionFailed, CommandRunsTooLong, NoSuchCommand
 
 
 class CommandExecutionResult(NamedTuple):
     """Result of shell command execution."""
 
-    stdout_data: bytes
+    stdout_data: str
     stderr_data: bytes
     exit_code: int
 
@@ -60,35 +51,11 @@ class ShellCommand:
                 f"{exit_code} and stderr:\n{stderr}"  # type: ignore
             )
         return CommandExecutionResult(
-            stdout_data=stdout, stderr_data=stderr, exit_code=exit_code
+            stdout_data=self._preprocess_stdout_data(stdout),
+            stderr_data=stderr,
+            exit_code=exit_code,
         )
 
     def _preprocess_stdout_data(self, stdout_data: bytes) -> str:
         """Decode, strip, lower stdout data."""
         return stdout_data.decode().strip().lower()
-
-
-class GetGpsCommand(ShellCommand):
-    """Shell command for getting current GPS coordinates."""
-
-    # Reqex pattern for dictionary including latitude and longitude inside itself
-    lat_lon_dict_pattern = r"{.*}"
-
-    def parse_coordinates(self, get_gps_command_output: bytes) -> Coordinates:
-        """Return GPS coordinates from output of shell command."""
-        try:
-            output = self._preprocess_stdout_data(get_gps_command_output)
-        except UnicodeDecodeError as err:
-            raise CantGetGpsCoordinates(f"Can't decode shell command output:\n{err}")
-        try:
-            lat_lon_dict = json.loads(
-                re.search(self.lat_lon_dict_pattern, output).group()  # type: ignore
-            )
-        except (AttributeError, JSONDecodeError):
-            raise CantGetGpsCoordinates(
-                f"Shell command output:\n'{output}'\nhas no dictionary "
-                f"with latitude and longitute inside itself"
-            )
-        return Coordinates(
-            latitude=lat_lon_dict["latitude"], longitude=lat_lon_dict["longitude"]
-        )
