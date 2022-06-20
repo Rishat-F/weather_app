6e5e303bc35f1c51a056c92df3f0bba03453e872
Rishat Fayzullin <nilluziaf@gmail.com>
2022-06-20 20:58:39 +0300

    Bugfix: Fixed Circular Imports error
    GepGpsCoordinates class removed,
    parsing GPS coordinates function
    goes to coordinates.py module

--- coordinates.py
+++ coordinates.py
@@ -1,12 +1,15 @@
 """Getting current GPS coordinates."""
 
+import json
+import re
+from json.decoder import JSONDecodeError
 from typing import NamedTuple
 
 import config
 from exceptions import CantGetGpsCoordinates, CommandExecutionFailed
-from shell_commands import GetGpsCommand
+from shell_commands import ShellCommand
 
-GET_GPS_COMMAND = GetGpsCommand(executable="whereami", arguments=["-f", "json"])
+GET_GPS_COMMAND = ShellCommand(executable="whereami", arguments=["-f", "json"])
 
 
 class Coordinates(NamedTuple):
@@ -22,7 +25,7 @@ def get_gps_coordinates() -> Coordinates:
     return _round_coordinates(coordinates, 2)
 
 
-def _get_gps_coordinates_by_command(command: GetGpsCommand) -> Coordinates:
+def _get_gps_coordinates_by_command(command: ShellCommand) -> Coordinates:
     """Return GPS coordinates by shell command."""
     try:
         command_output, *_ = command.execute()
@@ -32,8 +35,30 @@ def _get_gps_coordinates_by_command(command: GetGpsCommand) -> Coordinates:
             f"{[command.executable, *command.arguments]} command.\n"
             f"{err}"
         )
-    coordinates = command.parse_coordinates(command_output)
+    except UnicodeDecodeError as err:
+        raise CantGetGpsCoordinates(f"Can't decode shell command output:\n{err}")
+    coordinates = _parse_coordinates(command_output)
     return coordinates
+
+
+def _parse_coordinates(get_gps_command_output: str) -> Coordinates:
+    """Return GPS coordinates from output of shell command."""
+    # Reqex pattern for dictionary including latitude and longitude inside itself
+    lat_lon_dict_pattern = r"{.*}"
+    try:
+        lat_lon_dict = json.loads(
+            re.search(
+                lat_lon_dict_pattern, get_gps_command_output
+            ).group()  # type: ignore
+        )
+    except (AttributeError, JSONDecodeError):
+        raise CantGetGpsCoordinates(
+            f"Shell command output:\n'{get_gps_command_output}'\nhas no dictionary "
+            f"with latitude and longitute inside itself"
+        )
+    return Coordinates(
+        latitude=lat_lon_dict["latitude"], longitude=lat_lon_dict["longitude"]
+    )
 
 
 def _round_coordinates(coordinates: Coordinates, ndigit: int = 4) -> Coordinates:
