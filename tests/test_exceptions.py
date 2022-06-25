2f7b7027ed92aef12158445162f0c3436b61aed3
Rishat Fayzullin <nilluziaf@gmail.com>
2022-06-25 21:47:20 +0300

    Ref(tests): tests docstrings refactored

--- tests/test_exceptions.py
+++ tests/test_exceptions.py
@@ -28,32 +28,32 @@ class TestCoordinatesModuleExceptions:
     """Test exceptions raising while getting current GPS coordinates."""
 
     def test_no_such_command(self) -> None:
-        """There is no command in system for getting current GPS coordinates."""
+        """If there is no command in system for getting current GPS coordinates."""
         assert isinstance(False, NoSuchCommand)
 
     def test_command_runs_too_long(self) -> None:
-        """Command for getting current GPS coordinates runs too long."""
+        """If command for getting current GPS coordinates runs too long."""
         assert isinstance(False, CommandRunsTooLong)
 
     def test_command_has_stderr(self) -> None:
-        """Command for getting current GPS coordinates ends with err in stderr."""
+        """If command for getting current GPS coordinates ends with err in stderr."""
         assert isinstance(False, CantGetGpsCoordinates)
 
     def test_command_has_wrong_exit_code(self) -> None:
-        """Command for getting current GPS coordinates returns code != 0."""
+        """If command for getting current GPS coordinates returns code != 0."""
         assert isinstance(False, CantGetGpsCoordinates)
 
     def test_undecodable_command_output(self) -> None:
-        """Command stdout is undecodable."""
+        """If command stdout is undecodable."""
         assert isinstance(False, CantGetGpsCoordinates)
 
     def test_command_output_has_no_lat_lon_dictionary(self) -> None:
-        """Command stdout has no dictionary with latitude and longitude."""
+        """If command stdout has no dictionary with latitude and longitude."""
         assert isinstance(False, CantGetGpsCoordinates)
 
     def test_command_output_has_incorrect_dictionary(self) -> None:
         """
-        Dictionary with latitude and longitude is incorrect for json.loads().
+        If dictionary with latitude and longitude is incorrect for json.loads().
 
         Example: '{'latitude': 50.55, 'longitude': 50.55, qwerty}'.
         """
@@ -66,9 +66,11 @@ class TestWeatherApiServiceExceptions:
     coordinates = Coordinates(latitude=50, longitude=50)
 
     def test_no_such_command(self, monkeypatch: MonkeyPatch) -> None:
-        """There is no command in system for getting weather by GPS coordinates."""
+        """If there is no command in system for getting weather by GPS coordinates."""
 
         class MonkeyPatchShellCommand(ShellCommand):
+            """Mock for ShellCommand.__init__ method."""
+
             def __init__(self, *args: Any, **kwargs: Any):
                 _, _ = args, kwargs
                 self.executable = "qwerty"
@@ -80,16 +82,16 @@ class TestWeatherApiServiceExceptions:
             get_weather(self.coordinates)
 
     def test_no_open_weather_api_key(self, monkeypatch: MonkeyPatch) -> None:
-        """There is no OPEN_WEATHER_API_KEY variable in your environment."""
+        """If there is no OPEN_WEATHER_API_KEY variable in your environment."""
         monkeypatch.setattr(config, "OPEN_WEATHER_API_KEY", None)
         with pytest.raises(NoOpenWeatherApiKey):
             get_weather(self.coordinates)
 
     def test_command_runs_too_long(self, monkeypatch: MonkeyPatch) -> None:
-        """Command for getting weather by GPS coordinates runs too long."""
+        """If command for getting weather by GPS coordinates runs too long."""
 
         class MonkeyPatchShellCommand(ShellCommand):
-            """Mock for ShellCommand __init__ method."""
+            """Mock for ShellCommand.__init__ method."""
 
             def __init__(self, **_: Any):
                 self.executable = "ping"
@@ -101,13 +103,13 @@ class TestWeatherApiServiceExceptions:
             get_weather(self.coordinates)
 
     def test_wrong_open_weather_api_key(self, monkeypatch: MonkeyPatch) -> None:
-        """There is wrong OPEN_WEATHER_API_KEY variable in your environment."""
+        """If there is wrong OPEN_WEATHER_API_KEY variable in your environment."""
         monkeypatch.setattr(config, "OPEN_WEATHER_API_KEY", "qwerty")
         with pytest.raises(ApiServiceError):
             get_weather(self.coordinates)
 
     def test_command_has_stderr(self, monkeypatch: MonkeyPatch) -> None:
-        """Command for getting weather by GPS coordinates ends with err in stderr."""
+        """If command for getting weather by GPS coordinates ends with err in stderr."""
 
         def monkeypatch_communicate(*args: Any, **kwargs: Any) -> tuple[Any, bytes]:
             """Mock for Popen.communicate method."""
@@ -119,7 +121,7 @@ class TestWeatherApiServiceExceptions:
             get_weather(self.coordinates)
 
     def test_command_has_wrong_exit_code(self, monkeypatch: MonkeyPatch) -> None:
-        """Command for getting weather by GPS coordinates returns code != 0."""
+        """If command for getting weather by GPS coordinates returns code != 0."""
 
         def monkeypatch_wait(*args: Any, **kwargs: Any) -> int:
             """Mock for Popen.wait method."""
@@ -131,12 +133,13 @@ class TestWeatherApiServiceExceptions:
             get_weather(self.coordinates)
 
     def test_undecodable_command_output(self, monkeypatch: MonkeyPatch) -> None:
-        """Command stdout is undecodable."""
+        """If command stdout is undecodable."""
         undecodable_bytes = b"\x80\x02\x03"
 
         def monkeypatch_communicate(
             *args: Any, **kwargs: Any
         ) -> tuple[Undecodable_bytes, Any]:
+            """Mock for Popen.communicate method."""
             _, _ = args, kwargs
             return (undecodable_bytes, None)
 
@@ -157,10 +160,10 @@ class TestWeatherApiServiceExceptions:
     def test_command_output_has_incorrect_dictionary(
         self, command_output: str, monkeypatch: MonkeyPatch
     ) -> None:
-        """Command stdout has no dictionary inside."""
+        """If command stdout has no dictionary inside."""
 
         def monkeypatch_execute(*args: Any, **kwargs: Any) -> tuple[str, Any, Any]:
-            """Mock for ShellCommand execute method."""
+            """Mock for ShellCommand.execute method."""
             _, _ = args, kwargs
             return (command_output, None, None)
 
@@ -199,10 +202,10 @@ class TestWeatherApiServiceExceptions:
     def test_command_output_has_invalid_dictionary(
         self, command_output: str, monkeypatch: MonkeyPatch
     ) -> None:
-        """Command stdout has dictionary without needed weather data."""
+        """If command stdout has dictionary without needed weather data."""
 
         def monkeypatch_execute(*args: Any, **kwargs: Any) -> tuple[str, Any, Any]:
-            """Mock for ShellCommand execute method."""
+            """Mock for ShellCommand.execute method."""
             _, _ = args, kwargs
             return (command_output, None, None)
 
